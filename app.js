1ac979c4c39fa043d882d3ce777d535c5c19fad3
st. anger <at-am@yandex.ru>
2020-05-17 17:42:05 +0300

    add cors

--- app.js
+++ app.js
@@ -29,10 +29,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
 
-app.use('/', (req, res, next) => {
-  console.log("Recieved")
-  next()
-})
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+
+  next();
+});
 
 app.get('/crash-test', () => {
   setTimeout(() => {
