f1b0c172a805fbe8d5952d7d696f9f96779145cc
st. anger <at-am@yandex.ru>
2020-05-17 17:47:06 +0300

    options

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(requestLogger);
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
 
   next();
 });
