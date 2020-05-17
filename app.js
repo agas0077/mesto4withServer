e951e1c15e7f1fe38991a34e3410f57742301840
st. anger <at-am@yandex.ru>
2020-05-17 18:06:56 +0300

    cors

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const { Joi, celebrate, errors } = require('celebrate');
-const { cors } = require('cors')
 
 const router = require('./routes/routes');
 const { mongooseConfig, PORT, DATABASE_URL } = require('./config');
@@ -17,11 +16,12 @@ const NotFoundError = require('./errors/NotFoundError')
 
 const app = express();
 
-app.use(cors({
-  origin: '*',
-}));
-
-app.options('*', cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+  next();
+});
 
 app.use(helmet());
 
