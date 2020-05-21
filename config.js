f4ba15826ed6360c5216f23b2b2e0acc6f0f4c3b
st. anger <at-am@yandex.ru>
2020-05-21 18:33:26 +0300

    secret key check

--- config.js
+++ config.js
@@ -7,8 +7,12 @@ module.exports.mongooseConfig = {
 
 const isProduction = process.env.NODE_ENV === 'production';
 
+console.log(isProduction)
+
 module.exports.PORT = process.env.PORT || 3000;
 
 module.exports.KEY = isProduction ? process.env.JWT_SECRET : 'SOME-SECRET-KEY';
+
+console.log(this.KEY)
 
 module.exports.DATABASE_URL = isProduction ? process.env.DATABASE_URL : 'mongodb://localhost:27017/mydb';
